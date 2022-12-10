710ef3942c9b8ea1a651cd30a715c336eda0f977
Amon <Omit>
2022-12-10 21:21:51 +0000

    small update dashTec

--- ER_Prototipo_2223/Backend/resources/views/layout/dashboardTecn.blade.php
+++ ER_Prototipo_2223/Backend/resources/views/layout/dashboardTecn.blade.php
@@ -97,9 +97,17 @@
                         <img class="img-fluid" src="assets/img/burlas/burlas.png" alt="..." />
                     </div>
                 </div>
+                <!--Ferramenta 4-->
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#erro">
+                        <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                            <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/burlas/erros.png" alt="..." />
             </div>
         </div>
-
+        
+    </div>
     </section>
    <!--Footer -->
     <footer class="footer text-center">
@@ -319,6 +327,72 @@
                                         <button type="button" class="btn btn-danger">Apagar</button>
                                         <button type="button" class="btn btn-warning">Editar</button>
                                          <button type="button" class="btn btn-success">Atribuir</button>
+                                    </div>
+                                    <div> <br> </div>
+                                <button class="btn btn-primary" data-bs-dismiss="modal">
+                                    <i class="fas fa-xmark fa-fw"></i>
+                                    Fechar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- -->
+    <div class="portfolio-modal modal fade" id="erro" tabindex="-1" aria-labelledby="erro" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+                <div class="modal-body text-center pb-5">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-8">
+                                <!-- Portfolio Modal - Title-->
+                                <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Lista de Erros:</h2>
+                                <!-- Icon Divider-->
+                                <div class="divider-custom">
+                                    <div class="divider-custom-line"></div>
+                                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                    <div class="divider-custom-line"></div>
+                                </div>
+                                <!-- Portfolio Modal - Image-->
+                                <img class="img-fluid rounded mb-5" src="assets/img/burlas/erros.png" width="300" height="300" alt="..." />
+                                <!-- Portfolio Modal - Text-->
+                                <p class="mb-4">Lista de Burlas Ativas</p>
+                                <table class="table table-dark table-striped">
+                                    <thead>
+                                      <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Nome</th>
+                                        <th scope="col">Tag</th>
+                                        <th scope="col">Tipo de erro</th>
+                                      </tr>
+                                    </thead>
+                                    <tbody>
+                                      <tr>
+                                        <th scope="row">1</th>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                      </tr>
+                                      <tr>
+                                        <th scope="row">2</th>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                      </tr>
+                                      <tr>
+                                        <th scope="row">3</th>
+                                        <td colspan="2"></td>
+                                        <td></td>
+                                      </tr>
+                                    </tbody>
+                                </table>
+
+                                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                                        <button type="button" class="btn btn-danger">Apagar</button>                                       
                                     </div>
                                     <div> <br> </div>
                                 <button class="btn btn-primary" data-bs-dismiss="modal">
